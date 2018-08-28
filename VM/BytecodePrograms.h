33156a01ee34d9f906ceaac48542d0079e09ba99
WadeMcCall <wimccall42@students.tntech.edu>
2018-08-28 07:53:55 -0600

    Fix test project not compiling when VM is an exe not a lib

--- VM/BytecodePrograms.h
+++ VM/BytecodePrograms.h
@@ -1,38 +1,4 @@
 #pragma once
 #include "VM.h"
 
-int VMFibonacciImp(int fibArg) {
-	WVM vm;
-	const int fib = 1; // Address of the beginning of the fib function
-	std::vector<int> fibonacci = {
-		// int fib(n) {
-		//     if(n == 0) return 0;
-		FUN, LOAD, -3, // Load the last function arg
-		LITERAL, 0, // push 0 onto stack
-		EQUAL, JMPF, 11, // check if N == 0. If not, jump to instruction 11
-		LITERAL, 0, RETF, // If N == 0, return 0
-		//     if(n < 3) return 1;
-		LOAD, -3, // Load the argument again
-		LITERAL, 3, // put 3
-		LESS_THAN, // check if 3 is less than N
-		JMPF, 21, // if N is not less than 3 goto 21
-		LITERAL, 1, RETF, // Return 1
-		//     else return fib(n-1) + fib(n-2);
-		LOAD, -3, // load the function arg again
-		LITERAL, 1, // PUT 1
-		SUB, // Put N - 1
-		CALLF, fib, 1, // CALL the fib function with 1 arg from stack
-		LOAD, -3, // Load N again
-		LITERAL, 2, // Put 2
-		SUB, // Put N - 2
-		CALLF, fib, 1, // CALL fib again
-		ADD, // ADD the return value from the 2 FIB calls
-		RETF,
-		END_FUN, // function delimeter
-		// Main () {
-		LITERAL, fibArg, // put our arg
-		CALLF, fib, 1, // Call fib(6)
-		// End. 
-	};
-	return vm.interpret(fibonacci);
-}
+int VMFibonacciImp(int fibArg);
--- WVMTest/test.cpp
+++ WVMTest/test.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "BytecodePrograms.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(8, VMFibonacciImp(6));
+TEST(BytecodePrograms, Fibonacci) {
+	EXPECT_EQ(8, VMFibonacciImp(6));
+	EXPECT_EQ(34, VMFibonacciImp(9));
+	EXPECT_EQ(13, VMFibonacciImp(7));
 }
